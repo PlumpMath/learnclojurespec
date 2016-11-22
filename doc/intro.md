83142607ddcc15c0bd20cc60c3cd4af849a8f4df
Dan Dees <dand@appsmiths.com>
2016-11-22 08:41:46 +0000

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to learnclojurespec
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/learnclojurespec/core_test.clj
@@ -0,0 +1,7 @@
+(ns learnclojurespec.core-test
+  (:require [clojure.test :refer :all]
+            [learnclojurespec.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
